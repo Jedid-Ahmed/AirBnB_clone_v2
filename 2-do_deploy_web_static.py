dec74e1d11296c63636373e076c5ac535b4c6e3e
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-10-27 22:36:34 +0300

    Commits

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ Usage:
 """
 from fabric.api import env, put, run
 import os.path
-env.hosts = ['35.229.54.225', '35.231.225.251']
+env.hosts = ['3.238.28.227', '100.25.139.237']
 
 
 def do_deploy(archive_path):
