cc1472b2f72eafa323884a4a11a3fee3847455b8
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-11-13 21:03:53 +0300

    Commits

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Flask framework"""
+# displays states
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+ip = '0.0.0.0'
+port = 5000
+
+
+@app.route('/states_list')
+def states_list():
+    # lists the states
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ teardown db"""
-    if storage is not None:
-        storage.close()
+def teardown(self):
+    # tears down app context
+    storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ list of state ids"""
-    data = storage.all(State)
-    return render_template('7-states_list.html', total=data.values())
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host=ip, port=port)
